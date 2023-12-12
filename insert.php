1e01ecceec07ce95fef00353351da3e0255afbea
jean-celeste <jeancarlotarrobago.celeste@bicol-u.edu.ph>
2023-12-13 00:03:46 +0800

    update

--- insert.php
+++ insert.php
@@ -16,21 +16,21 @@
         echo "Error creating table: " . $conn->error;
     }
 
-        //Checks if HTTP POST method is used
-        if (isset($_POST['submit'])) {
-            $user_name = $_POST['user_name'];
-            $password = $_POST['confirm_password'];
-            $email = $_POST['email'];
-            
-            //SQL query to insert data to 'accounts' table
-            $sql = "INSERT INTO accounts (username, password, email) VALUES ('$user_name', '$password', '$email')";
-    
-            if ($conn->query($sql)) {
-                echo "Record inserted successfully";
-            } else {
-                echo "Error: " . $sql . "<br>" . $conn->error;
-            }
+    //Checks if HTTP POST method is used
+    if (isset($_POST['submit'])) {
+        $user_name = $_POST['user_name'];
+        $password = $_POST['confirm_password'];
+        $email = $_POST['email'];
         
-        $conn->close();
-    }
+        //SQL query to insert data to 'accounts' table
+        $sql = "INSERT INTO accounts (username, password, email) VALUES ('$user_name', '$password', '$email')";
+
+        if ($conn->query($sql)) {
+            echo "Record inserted successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+    
+    $conn->close();
+}
 ?>
