4a4bc95bbb0c9b6a9f68d4addd548b537b7f6a11
michaeljamil <michaeljamilmangga.cervano@bicol-u.edu.ph>
2023-12-14 05:49:20 +0800

    Added two way registration

---
+++ employee_login.php
@@ -0,0 +1,35 @@
+<?php
+include "connect.php";
+
+//Selects database: attendance_db
+$conn ->select_db("majk_db");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['user_name'];
+    $password = $_POST['password'];
+
+    // Create the SQL query
+    $sql = "SELECT * FROM employee
+            WHERE username='$username'
+            AND password='$password'";
+
+    // Execute the query
+    $result = $conn->query($sql);
+
+    if ($result) {
+        // Check if any rows were returned
+        if ($result->num_rows > 0) {
+            include "menu.html";
+            
+        } else {
+            include "index.html";
+            echo '<script>displayModal("Invalid username or password");</script>';
+        }
+    } else {
+        echo "Error: " . $conn->error;
+    }
+
+    // Close the connection
+    $conn->close();
+}
+?>
--- insert.php
+++ insert.php
@@ -1,36 +1,52 @@
 <?php
-    include "connect.php";
+include "connect.php";
 
-    //Selects database: majk_db
-    $conn ->select_db("majk_db");
+// Selects database: majk_db
+$conn->select_db("majk_db");
 
-    //Create table "admin" if there is no existing table
-    $tableSql = "CREATE TABLE IF NOT EXISTS admin(
-                record_id int(6) AUTO_INCREMENT PRIMARY KEY,
-                username VARCHAR(50) NOT NULL,
-                password VARCHAR(50) NOT NUll,
-                email VARCHAR(50)
-            )";
-    //Catches error creating table
-    if (!$conn->query($tableSql)) {
-        echo "Error creating table: " . $conn->error;
+// Create tables "admin" and "employee" if they don't exist
+$tableSqlAdmin = "CREATE TABLE IF NOT EXISTS admin (
+                    record_id int(6) AUTO_INCREMENT PRIMARY KEY,
+                    username VARCHAR(50) NOT NULL,
+                    password VARCHAR(50) NOT NUll,
+                    email VARCHAR(50)
+                )";
+
+$tableSqlEmployee = "CREATE TABLE IF NOT EXISTS employee (
+                    record_id int(6) AUTO_INCREMENT PRIMARY KEY,
+                    username VARCHAR(50) NOT NULL,
+                    password VARCHAR(50) NOT NUll,
+                    email VARCHAR(50)
+                )";
+
+// Catches error creating tables
+if (!$conn->query($tableSqlAdmin) || !$conn->query($tableSqlEmployee)) {
+    echo "Error creating tables: " . $conn->error;
+}
+
+// Checks if HTTP POST method is used
+if (isset($_POST['submit'])) {
+    $user_name = $_POST['user_name'];
+    $password = $_POST['confirm_password'];
+    $email = $_POST['email'];
+    $role = $_POST['role'];
+
+    // SQL query to insert data based on the selected role
+    if ($role === 'admin') {
+        $sql = "INSERT INTO admin (username, password, email) VALUES ('$user_name', '$password', '$email')";
+    } elseif ($role === 'employees') {
+        $sql = "INSERT INTO employee (username, password, email) VALUES ('$user_name', '$password', '$email')";
+    } else {
+        echo "Invalid role selected";
+        exit; // Stop execution if an invalid role is selected
     }
 
-    //Checks if HTTP POST method is used
-    if (isset($_POST['submit'])) {
-        $user_name = $_POST['user_name'];
-        $password = $_POST['confirm_password'];
-        $email = $_POST['email'];
-        
-        //SQL query to insert data to 'admin' table
-        $sql = "INSERT INTO admin (username, password, email) VALUES ('$user_name', '$password', '$email')";
+    if ($conn->query($sql)) {
+        echo "Record inserted successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
 
-        if ($conn->query($sql)) {
-            echo "Record inserted successfully";
-        } else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
-        }
-    
     $conn->close();
 }
 ?>
